ccde46b5952229c6e2c2b48dac608af7ee556766
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2024-10-22 20:50:38 +0530

    Update painters_alllocation.java

--- painters_alllocation.java
+++ painters_alllocation.java
@@ -1,49 +1,64 @@
-package codes;
-
 import java.util.*;
 
-public class painters_alllocation {
-
+public class painter_allocation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int p = sc.nextInt();
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        int t = sc.nextInt();  // number of test cases
+        for (int i = 0; i < t; i++) {
+            int num_boards = sc.nextInt();
+            int[] boards = new int[num_boards];  // lengths of boards
+            int num_painters = sc.nextInt();  // number of painters
+            for (int j = 0; j < num_boards; j++) {
+                boards[j] = sc.nextInt();
+            }
+            System.out.println(minimizeTime(boards, num_painters));
         }
-        Arrays.sort(arr);
-        System.out.println(time(arr, p));
     }
 
-    public static int time(int arr[], int p) {
-        int low = arr[0];
-        int high = arr[arr.length - 1];
-        int result = 0;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (isPossible(arr, p, mid)) {
-                result = mid;
-                low = mid + 1;
+    public static int minimizeTime(int[] boards, int num_painters) {
+        int lo = 0;
+        int hi = 0;
+
+        // sum of all boards (hi), and finding the maximum board length (can be set as lo)
+        for (int i = 0; i < boards.length; i++) {
+            hi += boards[i];
+            lo = Math.max(lo, boards[i]);  // lo could be the maximum board length
+        }
+
+        int ans = 0;
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
+            // check if it's possible to paint all boards within 'mid' time
+            if (isPossible(boards, num_painters, mid)) {
+                ans = mid;  // if possible, this could be our answer
+                hi = mid - 1;
             } else {
-                high = mid - 1;
+                lo = mid + 1;  // try a larger time
             }
         }
 
-        return result;
+        return ans;  // minimum time required to paint all boards
     }
 
-    public static boolean isPossible(int[] arr, int p, int time) {
-        int painter = 1;
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] + sum <= time) {
-                sum = sum + arr[i];
+    static boolean isPossible(int[] boards, int num_painters, int max_time) {
+        int painters_required = 1;
+        int time_spent = 0;
+
+        for (int i = 0; i < boards.length; i++) {
+            if (time_spent + boards[i] <= max_time) {
+                time_spent += boards[i];  // current painter continues painting
             } else {
-                painter++;
-                sum = arr[i];
+                painters_required++;  // need a new painter
+                time_spent = boards[i];  // the new painter starts with the current board
+            }
+
+            // if painters required exceed the available painters, return false
+            if (painters_required > num_painters) {
+                return false;
             }
         }
-        return painter <= p;
+
+        return true;
     }
 }
